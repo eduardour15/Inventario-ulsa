8c3a3e92280576ee7f9ad859bfa3369107f36c45
HarmonySpace <jchavaspace@gmail.com>
2022-04-21 15:55:14 -0600

    index.c

--- index.c
+++ index.c
@@ -1,7 +1,26 @@
 #include "functions.h"
 
+char nomb[] = "Empresa";
+
 int main(int argc, char const *argv[])
 {   
-   
+   printf("Bienvenido ");
+   printf("%s\n\n\n\n\n", nomb);
+    j = 0;
+    product *f;
+    f = malloc(sizeof(product));
+    if (f == NULL){
+        error();
+    }
+    while(1){
+        int i = 0;
+        fflush(stdin);
+        i = menu();
+        switch(i){
+            case 1: add_product(*f,); 
+                    break;
+        }
+    }
+
     return 0;
 }
